1f265425b12984dbe6ac9307046b49978d8ff32d
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-05-17 17:47:39 +0200

    feat(useState): exercice 4-2 event done

--- app/training/[...path]/exercices/use-state/4.jsx
+++ app/training/[...path]/exercices/use-state/4.jsx
@@ -3,7 +3,7 @@
 
 import clsx from "clsx";
 
-const Button = ({ children, variant, id }) => {
+const Button = ({ children, variant, id, ...props }) => {
   const buttonClass = clsx("btn ring-offset-2 ring-offset-base-100", {
     "btn-primary": variant === "primary",
     "btn-error": variant === "error",
@@ -12,7 +12,7 @@ const Button = ({ children, variant, id }) => {
   });
 
   return (
-    <button className={buttonClass} id={id}>
+    <button className={buttonClass} id={id} onClick={() => props.onClick?.(id)}>
       {children}
     </button>
   );
@@ -22,23 +22,27 @@ export default function App() {
 
   const handleClick = (e) => {
     if (e.target === e.currentTarget) return alert("You clicked on the container");
-    return alert(`You clicked on <${e.target.id}>`);
+    return
+  }
+
+  const handleBtnClick = (id) => {
+    alert(`You clicked on <${id}>`);
   }
 
   return (
     <div
       className="flex flex-wrap gap-4 p-4" onClick={(e) => handleClick(e)}
     >
-      <Button variant={"primary"} id="eat-me">
+      <Button variant={"primary"} id="eat-me" onClick={(id) => handleBtnClick(id)}>;
         Eat me
       </Button>
-      <Button variant={"error"} id="love-me">
+      <Button variant={"error"} id="love-me" onClick={(id) => handleBtnClick(id)}>
         Love me
       </Button>
-      <Button variant={"success"} id="drink-me">
+      <Button variant={"success"} id="drink-me" onClick={(id) => handleBtnClick(id)}>
         Drink me
       </Button>
-      <Button variant={"warning"} id="leave-me">
+      <Button variant={"warning"} id="leave-me" onClick={(id) => handleBtnClick(id)}>
         Eat me
       </Button>
     </div>
